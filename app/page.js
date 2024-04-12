6b377033a25101048fbc46187bbecc5282db344b
0xdblok <dblok0x@gmail.com>
2024-04-12 16:11:32 +0200

    ee

--- app/page.js
+++ app/page.js
@@ -1,9 +1,4 @@
 import Image from "next/image";
-import Navbar from "./component/navbar";
-import Hero from "./component/hero";
-import Footer from "./component/footer";
-import Plan from "./component/plan";
-import Cooking from "./component/cooking";
 
 import Another from "./component/another";
 import Formula from "./comp/formula";
