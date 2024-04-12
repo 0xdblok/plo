e11527c0a3fdf55be6180484d7b983e9603aa7c8
0xdblok <dblok0x@gmail.com>
2024-04-12 16:14:41 +0200

    zzz

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 
-import Another from "./component/another";
 import Formula from "./comp/formula";
 import Heroo from "./comp/heroo";
 import Solution from "./comp/solution";
